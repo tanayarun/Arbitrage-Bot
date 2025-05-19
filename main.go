f74b5fe1bc86383d202693bec4f815d31000961f
Tanay <ankulwartanay@gmail.com>
2025-05-20 01:54:55 +0530

    file structure

--- main.go
+++ main.go
@@ -1,40 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/go-resty/resty/v2"
-	"github.com/joho/godotenv"
-)
+import "fmt"
 
 func main() {
+	fmt.Println("sports book arb bot")
 	
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	apiKey := os.Getenv("API_KEY")
-	apiSecret := os.Getenv("API_SECRET")
-
-	fmt.Println("✅ Loaded API Key and Secret (not used yet)")
-	fmt.Println("API_KEY:", apiKey)
-	fmt.Println("API_SECRET:", apiSecret)
-
-	
-	baseURL := "https://testnet.binancefuture.com"
-	symbol := "BTCUSDT"
-
-	client := resty.New()
-	resp, err := client.R().
-		Get(baseURL + "/fapi/v1/ticker/price?symbol=" + symbol)
-
-	if err != nil {
-		log.Fatal("Error fetching price:", err)
-	}
-
-	fmt.Println("✅ Latest Price of", symbol + ":")
-	fmt.Println(resp.String())
 }
